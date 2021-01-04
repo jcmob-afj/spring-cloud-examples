d082ae6fc79473c37d43f49917a27832a0e0fe26
jcmob-afj <987232424@qq.com>
2021-01-04 18:26:53 +0800

    新增动态路由设置

---
+++ spring-cloud-gateway/cloud-gateway-eureka/src/main/java/com/neo/Routes/GatewayRouteDefinition.java
@@ -0,0 +1,71 @@
+package com.neo.Routes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author afj
+ * @date 2021/1/4 18:13
+ */
+public class GatewayRouteDefinition {
+    /**
+     * 路由的Id
+     */
+    private String id;
+    /**
+     * 路由断言集合配置
+     */
+    private List<GatewayPredicateDefinition> predicates = new ArrayList<>();
+    /**
+     * 路由过滤器集合配置
+     */
+    private List<GatewayFilterDefinition> filters = new ArrayList<>();
+    /**
+     * 路由规则转发的目标uri
+     */
+    private String uri;
+    /**
+     * 路由执行的顺序
+     */
+    private int order = 0;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<GatewayPredicateDefinition> getPredicates() {
+        return predicates;
+    }
+
+    public void setPredicates(List<GatewayPredicateDefinition> predicates) {
+        this.predicates = predicates;
+    }
+
+    public List<GatewayFilterDefinition> getFilters() {
+        return filters;
+    }
+
+    public void setFilters(List<GatewayFilterDefinition> filters) {
+        this.filters = filters;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+}
